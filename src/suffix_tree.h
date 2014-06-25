76e1dbbf14688e6b7c8269966beb48e8ad1e0570
latticetower <merlettaia@gmail.com>
2014-06-25 17:04:07 +0400

    before changing suffix_tree.h

--- src/suffix_tree.h
+++ src/suffix_tree.h
@@ -1,48 +1,58 @@
 #pragma once
-#include <memory>
 #include <set>
 #include <map>
 #include <algorithm>
 #include <vector>
 #include <sstream>
-#include "ivisitor.h"
+#include <boost/shared_ptr.hpp>
+
+class tree_vertex;
+
+typedef boost::shared_ptr<tree_vertex> tree_vertex_shared_ptr;
 
 class tree_vertex {
+  private:
+    //char letter;
+    bool is_root;
+    std::map<char, tree_vertex_shared_ptr > children; // really im not sure now what to store
+    std::vector<std::pair<size_t, size_t> > positions;
   public:
-    //typedef std::map<char, std::shared_ptr<tree_vertex> >::iterator ChildIterator;
+    //typedef std::map<char, tree_vertex_shared_ptr >::iterator ChildIterator;
     tree_vertex(): is_root(true) {}
     tree_vertex(char l): is_root(false) {}
-
+  public:
     //method returns pointer to found or newly created object
     tree_vertex* add_next(char l) {
-      std::map<char, std::shared_ptr<tree_vertex> >::iterator iter = children.find(l);
+      std::map<char, tree_vertex_shared_ptr >::iterator iter = children.find(l);
       if (iter != children.end()) {
         return iter->second.get();
       }
       else {
-        children[l] = std::shared_ptr<tree_vertex>(new tree_vertex(l));
+        children[l] = tree_vertex_shared_ptr(new tree_vertex(l));
         return children[l].get();
       }
     }
 
     tree_vertex* find_next(char l) {
-      std::map<char, std::shared_ptr<tree_vertex> >::iterator iter = children.find(l);
-      if (iter!= children.end()) {
+      std::map<char, tree_vertex_shared_ptr >::iterator iter = children.find(l);
+      if (iter != children.end()) {
         return iter->second.get();
       }
       return NULL;
     }
 
+  public:
     //method collects and returns information about all occurencies - string_no and position
     std::vector<std::pair<size_t, size_t> > get_all_pairs() {
       std::vector<std::pair<size_t, size_t> > result;
       result.insert(result.begin(), positions.begin(), positions.end());
-      for (std::map<char, std::shared_ptr<tree_vertex> >::iterator iter = children.begin(); iter != children.end(); ++iter) {
+      for (std::map<char, tree_vertex_shared_ptr >::iterator iter = children.begin(); iter != children.end(); ++iter) {
         std::vector<std::pair<size_t, size_t> > r = iter->second->get_all_pairs();
         result.insert(result.begin(), r.begin(), r.end());
       }
       return result;
     }
+
     void clear() {
       children.clear();
     }
@@ -50,18 +60,12 @@ class tree_vertex {
     void add_info(size_t string_no, size_t index) {
       positions.push_back(std::make_pair(string_no, index));
     }
-  private:
-    //char letter;
-    bool is_root;
-    std::map<char, std::shared_ptr<tree_vertex> > children; // really im not sure now what to store
-    std::vector<std::pair<size_t, size_t> > positions;
-
-  //    std::set<size_t> start_positions;
 };
-class SuffixTree{
+
+class SuffixTree {
   protected:
     void addSuffix(size_t string_no, size_t index) {
-      tree_vertex<SuffixTree> * next = &root;
+      tree_vertex * next = &root;
       for (size_t i = index; i < string_container[string_no].size(); i++){
         next = next->add_next(string_container[string_no][i]);
       }
@@ -74,17 +78,17 @@ class SuffixTree{
       string_container.clear();
       root.clear();
     }
-    void addString(std::string const & str) {
-      std::string _str = std::string(str.c_str());
-      string_container.push_back(_str);
+    void addString(std::string const & reference_str, std::string const & str) {
+      string_container.push_back(str);
+      references_container.push_back(reference_str);
       for (size_t i = 0; i < _str.length(); i++) {
         addSuffix(string_container.size() - 1, i);
       }
     }
 
-    std::map<std::string, std::set<size_t> > findPattern(std::string const& pattern) {
-      std::map<std::string, std::set<size_t> > result;
-      tree_vertex<SuffixTree> * next = &root;
+    std::set<std::string > findPattern(std::string const& pattern) {
+      std::set<std::string> result;
+      tree_vertex * next = &root;
       for (size_t i = 0; i < pattern.size(); i++){
         next = next->find_next(pattern[i]);
         if (next == NULL)
@@ -92,18 +96,11 @@ class SuffixTree{
       }
       std::vector<std::pair<size_t, size_t> > all_pairs = next->get_all_pairs();
       for (std::vector<std::pair<size_t, size_t> >::iterator iter = all_pairs.begin(); iter != all_pairs.end(); ++ iter) {
-        std::map<std::string, std::set<size_t> >::iterator iter2 = result.find(string_container[iter->first]);
-        if (iter2 == result.end()) {
-
-        }
-        else {
-
-        }
-        result[string_container[iter->first]].insert(iter->second);
+        result.insert(references_container[iter->first]);
       }
       return result;
     }
   private:
-    std::vector<std::string> string_container;
-    tree_vertex<SuffixTree> root;
+    std::vector<std::string> string_container, references_container;
+    tree_vertex root;
 };
